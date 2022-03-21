4bafa561e317dc5350192d9f703dfa76da88ce09
Jean Paul <jeanpaul2022@gmail.com>
2022-03-20 23:35:38 -0300

    Added new component Tap in App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { PanGesture } from "./src";
+import { PanGesture, TapGesture } from "./src";
+import { colors } from "./src/Config";
 import "react-native-gesture-handler";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <PanGesture />
+      {/* <PanGesture /> */}
+      <TapGesture />
     </View>
   );
 }
@@ -15,6 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.yellow,
   },
 });
